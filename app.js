82b0133fa137dd73445609460a863cd7745b3808
Hayden Cleaver <rexcleaver90@gmail.com>
2022-05-25 19:56:19 -0400

    I don't even remember anymore

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ let userDrink = prompt("How about a favorite drink?");
 if (userDrink == "Water"){
     document.write(" Hey, that's my number one drink too!");
 } else {
-    document.write("Nice! I'm more of a water kinda guy myself.");
+    document.write(userDrink + ", nice! I'm more of a water kinda guy myself.");
 }
 
 document.write(" Well, here are some of my regular meals:");
+
