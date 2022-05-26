2382636c852620e39a39d363f83dc6d36c080e1c
Hayden Cleaver <rexcleaver90@gmail.com>
2022-05-26 00:07:05 -0400

    fixing logic errors; finalizing colors

--- app.js
+++ app.js
@@ -8,20 +8,20 @@ if (userColor !==""){
 let userFood = prompt("What's your favorite food?");
 console.log(userFood);
 
-if (userFood == "rice" || "Rice"){
+if (userFood == "rice" || userFood == "Rice"){
     document.write(" I love rice too! ");
-} else if (userFood == "brussel sprouts" || "Brussel Sprouts") {
+} else if (userFood == "brussel sprouts" || userFood =="Brussel Sprouts"){
     document.write(" Gross.");
 } else {
-    document.write(userFood + " is great! But it's not as good as rice.\n");
+    document.write(userFood + " is great! But it's not as good as rice. ");
 }
 
 let userDrink = prompt("How about a favorite drink?");
 
-if (userDrink == "Tea" || "tea"){
+if (userDrink == "Tea" || userDrink == "tea"){
     document.write("Hey, that's my number one drink too!");
 } else {
-    document.write(userDrink + ", nice! I'm more of a tea kinda guy myself.");
+    document.write(userDrink + ", huh? Nice! I'm more of a tea kinda guy myself.");
 }
 
 document.write(" Well, here are some of my regular meals:");
