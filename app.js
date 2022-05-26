500102fc1785493c62843d4af19ca3b67a4497cd
Hayden Cleaver <rexcleaver90@gmail.com>
2022-05-26 15:18:41 -0400

    first draft of functions

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-let userColor = prompt("What's your favorite color?");
+function colorAnswer(){
+    
+    let userColor = prompt("What's your favorite color?");
     console.log(userColor);
     
-document.body.style.backgroundColor = userColor;
+    document.body.style.backgroundColor = userColor;
+};
 
-let userFood = prompt("What's your favorite food?");
-console.log(userFood);
+function foodAnswer() {
+    
+    let userFood = prompt("What's your favorite food?");
+    console.log(userFood);
 
-if (userFood == "rice" || userFood == "Rice"){
-    document.write(" I love rice too! ");
-} else if (userFood == "brussel sprouts" || userFood =="Brussel Sprouts"){
-    document.write(" Gross.");
-} else {
-    document.write(userFood + " is great! But it's not as good as rice. ");
-}
+    if (userFood == "rice" || userFood == "Rice"){
+        document.write(" I love rice too! ");
+    } else if (userFood == "brussel sprouts" || userFood =="Brussel Sprouts"){
+        document.write(" Gross.");
+    } else {
+        document.write(userFood + " is great! But it's not as good as rice. ");
+    }
+};
 
-let userDrink = prompt("How about a favorite drink?");
+function drinkAnswer(){
+    
+    let userDrink = prompt("How about a favorite drink?");
 
-if (userDrink == "Tea" || userDrink == "tea"){
+    if (userDrink == "Tea" || userDrink == "tea"){
     document.write("Hey, that's my number one drink too!");
-} else {
+    } else {
     document.write(userDrink + ", huh? Nice! I'm more of a tea kinda guy myself.");
-}
+    }
 
-document.write(" Well, here are some of my regular meals:");
+    document.write(" If you're curious, here are some of my regular meals and a few tips on how to make them better:");
+};
