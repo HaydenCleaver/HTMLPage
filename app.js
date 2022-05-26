6b65eaad9cae1ccc1263494fa37b9a2446247f37
Hayden Cleaver <rexcleaver90@gmail.com>
2022-05-25 23:36:33 -0400

    finished color change prompt (with google help)

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+let userColor = prompt("What's your favorite color?");
+    console.log(userColor);
+
+if (userColor !==""){
+    document.body.style.backgroundColor = userColor;
+    }
+    
 let userFood = prompt("What's your favorite food?");
 console.log(userFood);
 
-if (userFood == "Rice"){
-    document.write(" Hey, I love rice too!");
-} else if (userFood == "Brussel Sprouts") {
+if (userFood == "rice" || "Rice"){
+    document.write(" I love rice too! ");
+} else if (userFood == "brussel sprouts" || "Brussel Sprouts") {
     document.write(" Gross.");
 } else {
     document.write(userFood + " is great! But it's not as good as rice.\n");
@@ -11,10 +18,10 @@ if (userFood == "Rice"){
 
 let userDrink = prompt("How about a favorite drink?");
 
-if (userDrink == "Water"){
-    document.write(" Hey, that's my number one drink too!");
+if (userDrink == "Tea" || "tea"){
+    document.write("Hey, that's my number one drink too!");
 } else {
-    document.write(userDrink + ", nice! I'm more of a water kinda guy myself.");
+    document.write(userDrink + ", nice! I'm more of a tea kinda guy myself.");
 }
 
 document.write(" Well, here are some of my regular meals:");
